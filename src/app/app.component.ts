2e86b32c7b1866018cac78eab236cead8e61adf6
Gobinda Paul <gpaul@vendasta.com>
2025-03-24 11:30:34 -0600

    Adding new wish items automatically into the list by taking input from the user.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,12 +15,21 @@ export class AppComponent {
     new WishItem('Get coffee', true),
     new WishItem('Find grass that cuts itself'),
   ];
+
+  // this variable will contains all the new wish text. and by accessing this variable
+  // we will add the new text to the wish list.
+  newWishText: string = '';
+
   title = 'wishlist';
+
+  addNewWishButtonClicked() {
+    if (this.newWishText.length > 0) {
+      this.wishes.push(new WishItem(this.newWishText));
+      this.newWishText = '';
+    }
+  }
 
   toggleWishitemCompletion(item: WishItem) {
     item.isComplete = !item.isComplete;
-    console.log(item);
-
-    console.log(this.wishes);
   }
 }
