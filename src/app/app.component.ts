75aabd85b6ea7516a7e52422018bcf217a6995e4
Gobinda Paul <gpaul@vendasta.com>
2025-03-24 11:51:59 -0600

    using ngModelChange action with html selector - so that when the selector gets changed, we can handle the event.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,17 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself'),
   ];
 
+  // this variable contains all the visible wishes according to the filter that user
+  // have selected like if filter = all, then this variable will have all the wishes in it.
+  visibleItems : WishItem[] = this.wishes;
+
   // this variable will contains all the new wish text. and by accessing this variable
   // we will add the new text to the wish list.
   newWishText: string = '';
+
+  // this variable will hold the filter type selected for showing wish list.
+  // filter can have values like (All, Fulfilled, Unfulfilled)
+  wishListFilter : string = '0';
 
   title = 'wishlist';
 
@@ -26,10 +34,26 @@ export class AppComponent {
     if (this.newWishText.length > 0) {
       this.wishes.push(new WishItem(this.newWishText));
       this.newWishText = '';
+      this.recalculateVisibleItems();
     }
   }
 
   toggleWishitemCompletion(item: WishItem) {
     item.isComplete = !item.isComplete;
+    this.recalculateVisibleItems();
+  }
+
+  wishListFilterChanged(event: any) {
+    this.recalculateVisibleItems();
+  }
+
+  recalculateVisibleItems() {
+    if (this.wishListFilter === '0') {
+      this.visibleItems = this.wishes;
+    } else if (this.wishListFilter === '1') {
+      this.visibleItems = this.wishes.filter(item => !item.isComplete);
+    } else {
+      this.visibleItems = this.wishes.filter(item => item.isComplete);
+    }
   }
 }
