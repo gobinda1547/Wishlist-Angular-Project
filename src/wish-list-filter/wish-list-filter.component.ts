8467d80d3bb7bba7d4d55a22f09fd0768fddc01a
Gobinda Paul <gpaul@vendasta.com>
2025-03-24 14:53:33 -0600

    Added a new component wish list filter, it will just hold the filter option

---
+++ src/wish-list-filter/wish-list-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wish-list-filter',
+  imports: [],
+  templateUrl: './wish-list-filter.component.html',
+  styleUrl: './wish-list-filter.component.css'
+})
+export class WishListFilterComponent {
+
+}
