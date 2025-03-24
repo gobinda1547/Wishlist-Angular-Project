64ac7250333c23431e41e96108801924be06907a
Gobinda Paul <gpaul@vendasta.com>
2025-03-24 10:12:25 -0600

    Display hardcoded wish list items in the template using ng directives. For using the ng directives we had to import the common module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  wishes = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Find grass that cuts itself'),
+  ];
   title = 'wishlist';
 }
