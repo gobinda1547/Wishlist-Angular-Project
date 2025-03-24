90804c84fd95fb59d9115de692cec3047e726399
Gobinda Paul <gpaul@vendasta.com>
2025-03-24 13:53:36 -0600

    Created add wish form sub-component by using angular cli.

---
+++ src/add-wish-form/add-wish-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-wish-form',
+  imports: [],
+  templateUrl: './add-wish-form.component.html',
+  styleUrl: './add-wish-form.component.css'
+})
+export class AddWishFormComponent {
+
+}
